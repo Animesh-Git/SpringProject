b3ca11e49a4c89f635fe10ccd0becb36928bac44
Animesh Behera <supersonu086@gmail.com>
2025-07-07 08:40:06 +0530

    second commit

--- MyTestApp/src/main/java/com/example/demo/MyTestAppApplication.java
+++ MyTestApp/src/main/java/com/example/demo/MyTestAppApplication.java
@@ -8,6 +8,7 @@ public class MyTestAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyTestAppApplication.class, args);
+		System.out.println("Hello world");
 	}
 
 }
